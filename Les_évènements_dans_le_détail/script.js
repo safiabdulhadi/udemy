8466268cf2ccbdd1df5a314c88ea9cc60d65b3b0
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-11-16 09:23:54 +0100

    first commit

---
+++ Les_évènements_dans_le_détail/script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('button');
+
+// btn.onclick = function(){
+//     console.log("hello world");
+// }
+// btn.addEventListener('click',() =>{
+// console.log('helo world clicked');
+// });
+
+// btn.addEventListener('click',foo);
+
+// function foo(){
+//     console.log("Hello world!");
+//     btn.removeEventListener('click',foo);
+// }
